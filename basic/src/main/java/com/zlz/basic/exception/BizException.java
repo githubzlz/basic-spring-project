2bb5714ef17a002ceb66f6987167074f46a04f05
朱林忠 <wb_zhulinzhong@meituan.com>
2022-04-02 16:07:15 +0800

    ✅ 异常定义修改⚡ ️

--- basic/src/main/java/com/zlz/basic/exception/BizException.java
+++ basic/src/main/java/com/zlz/basic/exception/BizException.java
@@ -8,6 +8,8 @@ package com.zlz.basic.exception;
  */
 public class BizException extends RuntimeException {
 
+    protected Integer code = 0;
+
     public BizException() {
     }
 
@@ -19,11 +21,25 @@ public class BizException extends RuntimeException {
         super(message, cause);
     }
 
+    public BizException(Integer code, String message) {
+        super(code + ":" + message);
+        this.code = code;
+    }
+
+    public BizException(Integer code, String message, Throwable cause) {
+        super(code + ":" + message, cause);
+        this.code = code;
+    }
+
     public BizException(Throwable cause) {
         super(cause);
     }
 
     public BizException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public Integer getCode() {
+        return code;
     }
 }
